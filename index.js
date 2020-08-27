7e07d9d0ac5180d32f2175bfb89abf581aae5798
Leo <leoaguilarsanchez97@gmail.com>
2020-08-27 15:39:46 -0600

    Agregada API nodejs

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('API ERPPLUS WORKING');
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`API iniciada en el puerto ${port}`);
+});
